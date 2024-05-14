1b8f49befd5c543b8571eda65482b7e6bb09e952
acridBeaver <71394632+acridBeaver@users.noreply.github.com>
2024-05-14 12:10:08 +0500

    Update task.c

--- task.c
+++ task.c
@@ -8,14 +8,6 @@
 #include <math.h>
 #include <sys/types.h>
 
-char *getconcat(int offset, int zerocount)
-{
-    char *soffset = malloc(offset == 0 ? 1 : (int)log10(offset) + 1);
-    char *szero = malloc((int)log10(zerocount) + 1);
-    sprintf(soffset, "%d", offset);
-    sprintf(szero, "%d", zerocount);
-    return strcat(strcat(soffset, " "), strcat(szero, "\n"));
-}
 
 bool iszeroed(char buf[], int size)
 {
@@ -63,63 +55,35 @@ int main(int argc, char *argv[])
     // читаем с stdin, если первый позиционный - последний
     int sourcefd;
     int targetfd;
-    int targetmetafd;
+    //int targetmetafd;
     if (optind < argc - 1)
     {
         sourcefd = open(argv[optind], O_RDONLY);
         targetfd = open(argv[optind + 1], O_CREAT | O_WRONLY | O_TRUNC, 0777);
-        targetmetafd = open(strcat(argv[optind + 1], ".meta"), O_CREAT | O_WRONLY | O_TRUNC, 0777);
+        //targetmetafd = open(strcat(argv[optind + 1], ".meta"), O_CREAT | O_WRONLY | O_TRUNC, 0777);
     }
     else
     {
         sourcefd = STDIN_FILENO;
         targetfd = open(argv[optind], O_CREAT | O_WRONLY | O_TRUNC, 0777);
-        targetmetafd = open(strcat(argv[optind], ".meta"), O_CREAT | O_WRONLY | O_TRUNC, 0777);
+      //  targetmetafd = open(strcat(argv[optind], ".meta"), O_CREAT | O_WRONLY | O_TRUNC, 0777);
     }
 
     int ibufsize = atoi(bufsize);
 
     char buf[ibufsize];
-    int bytesRead = read(sourcefd, buf, ibufsize);
 
-    int zerocount = 0;
-    int offset = 0;
-    int overallbytes = 0;
-    while (bytesRead != 0)
-    {
-        overallbytes += bytesRead;
-        if (iszeroed(buf, bytesRead))
-        {
-            if (zerocount == 0)
-            {
-                offset = overallbytes - bytesRead;
-            }
-            zerocount += bytesRead;
-        }
-        else
-        {
-            write(targetfd, buf, bytesRead);
-            if (zerocount > 0)
-            {
-                int len = offset == 0 ? 1 : (int)log10(offset) + 1;
-                len += (int)log10(zerocount) + 3;
-                char *strtowrite = getconcat(offset, zerocount);
-                write(targetmetafd, strtowrite, len);
-            }
-            zerocount = 0;
-        }
-        bytesRead = read(sourcefd, buf, ibufsize);
-    }
-    if (zerocount > 0)
-    {
-        int len = offset == 0 ? 1 : (int)log10(offset) + 1;
-        len += (int)log10(zerocount) + 3;
-        char *strtowrite = getconcat(offset, zerocount);
-        write(targetmetafd, strtowrite, len);
-    }
+	while (ibufsize = read(sourcefd, buf, ibufsize)) {
+		if (iszeroed(buf, ibufsize)) {
+			lseek(targetfd, ibufsize, SEEK_CUR);
+		}
+		else {
+			write(targetfd, buf, ibufsize);
+		}
+	}
     close(sourcefd);
     close(targetfd);
-    close(targetmetafd);
+    //close(targetmetafd);
 
     return 0;
 }
